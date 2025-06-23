4806e80d9023c8c5a2a6216841a1715aa34e785d
LukeAWeaver <zambusto@gmail.com>
2025-06-22 22:40:10 -0500

    removed unused nav bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -26,15 +26,6 @@ function App() {
     }}>
       <AppBar position="static" elevation={0}>
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             AI Feature Explorer
           </Typography>

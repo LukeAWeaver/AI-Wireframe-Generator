b6694eb74b22189df60c25104749f44240d11f00
LukeAWeaver <zambusto@gmail.com>
2025-07-03 19:50:42 -0500

    UX enhancements hotfix#2

--- frontend/src/compound/home/HomeContent.tsx
+++ frontend/src/compound/home/HomeContent.tsx
@@ -3,6 +3,6 @@ export const HomeContent = () => (
   <Stack gap={8}>
   <Typography variant="h4">Welcome to my Portfolio!</Typography>
   <Typography>This portfolio is a full-stack web app hosted on Render for free ( more details in the ${"'" + "Projects" + "'"} section).</Typography>
-  <Typography>This portfolio is also hosting a ${"'" + 'Wireframe Generator' + "'"} side project that I'm working on so that I can keep my skills up and develop something interesting.</Typography>
+  <Typography>This portfolio is also hosting a ${"'" + "Wireframe Generator" + "'"} side project that I'm working on so that I can keep my skills up and develop something interesting.</Typography>
   </Stack>
 ); 

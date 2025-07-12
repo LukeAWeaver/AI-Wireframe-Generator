0bc97b3126338d7e0a0e996501e1566f3eb42925
LukeAWeaver <zambusto@gmail.com>
2025-07-12 18:04:09 -0500

    compile fixes

--- frontend/src/compound/wireframe/WireframeGeneratorPanel.tsx
+++ frontend/src/compound/wireframe/WireframeGeneratorPanel.tsx
@@ -1,13 +1,9 @@
-import { useState } from 'react';
 import { Box } from '@mui/material';
 
 export const WireframeGeneratorPanel = () => {
-  const [input, setInput] = useState('');
-  const [wireframe, setWireframe] = useState<string | null>(null);
 
   return (
     <Box>
-      {/* Render the main wireframe generator UI here */}
     </Box>
   );
 } 
